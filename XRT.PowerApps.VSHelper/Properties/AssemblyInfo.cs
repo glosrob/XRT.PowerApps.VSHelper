f6e771ea4cdf3029173be660f896acb4123af4ff
Robert Tilling <robert.tilling@xrtsoft.net>
2025-07-18 20:39:08 +0100

    Copy vsixs for release builds locally

--- XRT.PowerApps.VSHelper/Properties/AssemblyInfo.cs
+++ XRT.PowerApps.VSHelper/Properties/AssemblyInfo.cs
@@ -1,33 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XRT.PowerApps.VSHelper")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("XRT.PowerApps.VSHelper")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("PowerApps VSHelper")]
+[assembly: AssemblyDescription("Helper for publishing and retrieving PowerApps web resources.")]
+[assembly: AssemblyCompany("XRTSoft")]
+[assembly: AssemblyProduct("PowerApps VSHelper for Visual Studio")]
+[assembly: AssemblyCopyright("Copyright © XRTSoft 2025")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
